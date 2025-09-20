7d6305d0213fca7a6e03073166ed79f353838424
HilqiaKenda <hilkiakenda@gmail.com>
2025-09-20 21:46:56 +0300

    fix the cors

--- service/client.ts
+++ service/client.ts
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL =
-  process.env.REACT_APP_API_URL ||
-  "https://fleettrack-backend-1.onrender.com/api/v1.1";
+const API_BASE_URL = "https://fleettrack-backend-1.onrender.com/api/v1.1";
 
 export const apiClient = axios.create({
   baseURL: API_BASE_URL,
